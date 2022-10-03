243bd6d96b55e87851426f8b418a148cca89ab38
Zeynepozden34 <114918310+Zeynepozden34@users.noreply.github.com>
2022-10-03 18:13:23 +0300

    ilk uygulama hello world

---
+++ Proje01_Hello_World/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Proje01_Hello_World
+{
+    internal class Program // Ana scop başlangıç noktası
+    {
+        static void Main(string[] args) // ilk çalışacak metot
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Ben Zeynep");
+        }
+    }
+}
