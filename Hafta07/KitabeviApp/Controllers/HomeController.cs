77d4a9961d85438b76c5128f2ce40c9b40c89196
Zeynepozden34 <114918310+Zeynepozden34@users.noreply.github.com>
2022-11-17 18:08:45 +0300

    17.11.2022

---
+++ Hafta07/KitabeviApp/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using KitabeviApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KitabeviApp.Controllers;
+
+public class HomeController : Controller
+{
+    KitabeviContext context = new KitabeviContext();
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult KategoriListesi()
+    {
+        var kategoriler = context.Kategoriler.ToList();
+        return View(kategoriler);
+    }
+    public IActionResult YazarListesi()
+    {
+        var yazarlar = context.Yazarlar.ToList();
+        return View(yazarlar);
+    }
+    public IActionResult KitapListesi(int? id=null)  // soru işareti kullanma nedenim id primary key olduğu için null bırakılamaz bu nedenle soru işareti koyduk.
+    {
+        List<Kitap> kitaplar=null;
+        if (id==null)
+        {
+            
+            kitaplar = context
+            .Kitaplar
+            .Include(k => k.Kategori)
+            .Include(k => k.Yazar)
+            .ToList();
+        }else
+        {
+        //AsQueryable to liste çevirmeden yapılması gereken birşey varsakullanılır sorgulama yapar.
+             kitaplar = context
+            .Kitaplar
+            .Where(K=>K.KategoriId==id)
+            .Include(k => k.Kategori)
+            .Include(k => k.Yazar)
+            .ToList();
+        
+        }
+        
+         return View(kitaplar); 
+    }
+    public IActionResult Detay(int id)
+    {
+        var kitap=context.Kitaplar
+        .Where(k=>k.Id==id)
+        .Include(k=>k.Yazar)
+        .Include(k=>k.Kategori)
+        .FirstOrDefault();
+
+
+        return View(kitap);
+    }
+
+}
---
+++ Hafta07/KitabeviApp/Migrations/20221117143045_KitapDegisti.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KitabeviApp.Migrations
+{
+    public partial class KitapDegisti : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AnaSayfa",
+                table: "Kitaplar",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Ozet",
+                table: "Kitaplar",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Kitaplar",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "AnaSayfa", "Ozet" },
+                values: new object[] { true, "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque temporibus labore ipsam magni dicta laborum similique quam cupiditate eius. Debitis nemo eius ducimus officiis corrupti dolorum expedita neque distinctio tempora!" });
+
+            migrationBuilder.UpdateData(
+                table: "Kitaplar",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "AnaSayfa", "Ozet" },
+                values: new object[] { true, "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque temporibus labore ipsam magni dicta laborum similique quam cupiditate eius. Debitis nemo eius ducimus officiis corrupti dolorum expedita neque distinctio tempora!" });
+
+            migrationBuilder.UpdateData(
+                table: "Kitaplar",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "AnaSayfa", "Ozet" },
+                values: new object[] { true, "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque temporibus labore ipsam magni dicta laborum similique quam cupiditate eius. Debitis nemo eius ducimus officiis corrupti dolorum expedita neque distinctio tempora!" });
+
+            migrationBuilder.UpdateData(
+                table: "Kitaplar",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "AnaSayfa", "Ozet" },
+                values: new object[] { true, "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque temporibus labore ipsam magni dicta laborum similique quam cupiditate eius. Debitis nemo eius ducimus officiis corrupti dolorum expedita neque distinctio tempora!" });
+
+            migrationBuilder.UpdateData(
+                table: "Kitaplar",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "AnaSayfa", "Ozet" },
+                values: new object[] { true, "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque temporibus labore ipsam magni dicta laborum similique quam cupiditate eius. Debitis nemo eius ducimus officiis corrupti dolorum expedita neque distinctio tempora!" });
+
+            migrationBuilder.UpdateData(
+                table: "Kitaplar",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "AnaSayfa", "Ozet" },
+                values: new object[] { true, "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque temporibus labore ipsam magni dicta laborum similique quam cupiditate eius. Debitis nemo eius ducimus officiis corrupti dolorum expedita neque distinctio tempora!" });
+
+            migrationBuilder.UpdateData(
+                table: "Kitaplar",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "AnaSayfa", "Ozet" },
+                values: new object[] { true, "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque temporibus labore ipsam magni dicta laborum similique quam cupiditate eius. Debitis nemo eius ducimus officiis corrupti dolorum expedita neque distinctio tempora!" });
+
+            migrationBuilder.UpdateData(
+                table: "Kitaplar",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "AnaSayfa", "Ozet" },
+                values: new object[] { true, "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque temporibus labore ipsam magni dicta laborum similique quam cupiditate eius. Debitis nemo eius ducimus officiis corrupti dolorum expedita neque distinctio tempora!" });
+
+            migrationBuilder.UpdateData(
+                table: "Kitaplar",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "AnaSayfa", "Ozet" },
+                values: new object[] { true, "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque temporibus labore ipsam magni dicta laborum similique quam cupiditate eius. Debitis nemo eius ducimus officiis corrupti dolorum expedita neque distinctio tempora!" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnaSayfa",
+                table: "Kitaplar");
+
+            migrationBuilder.DropColumn(
+                name: "Ozet",
+                table: "Kitaplar");
+        }
+    }
+}
