b6bfeb98ab9bba17280aa434c643834c0284cc66
Zeynepozden34 <114918310+Zeynepozden34@users.noreply.github.com>
2022-11-22 18:20:46 +0300

    22.11.2022

---
+++ Hafta08/Proje03/KitabeviApp/ViewModels/YazarViewModel.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KitabeviApp.ViewModels
+{
+    public class YazarViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Lütfen adı boş bırakmayınız.")]
+        [StringLength(30,ErrorMessage ="Lütfen 30 karakterden fazla girmeyiniz!!")]
+        [Display(Name ="Yazar Adı Soyadı:", Prompt ="Lütfen yazar ad soyadını giriniz...")]
+        public string Ad { get; set; }
+        [Required(ErrorMessage ="Lütfen doğum yılını boş bırakmayınız.")]
+        [Display(Name ="Yazar Doğum Yılı:", Prompt ="2000...")]
+        public int? DogumYili { get; set; }  
+        public char Cinsiyet { get; set; }
+    }
+}
