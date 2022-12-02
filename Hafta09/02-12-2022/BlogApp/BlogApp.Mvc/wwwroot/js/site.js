cb6d94c7ab79d062d6f483323c7b730fddfed97b
Zeynepozden34 <114918310+Zeynepozden34@users.noreply.github.com>
2022-12-02 18:01:29 +0300

    2.12.2022

---
+++ Hafta09/02-12-2022/BlogApp/BlogApp.Mvc/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿function convertToShortDate(datestring) {
+    const shortDate = new Date(datestring).toLocaleDateString('tr-Tr'); 
+}
+function convertFirstLetterToUpperCase(text) {
+    text = text.toString();
+    const convertedText = text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+    return convertedText;
+}
+
+// kategori eklerken true ve falseun ilk harflerini büyük yazmak, ve Oluşturulma tarihini sadece tarih yazılmasını istedik. 
