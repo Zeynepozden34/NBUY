ef72e9c30073f510e38b6040f1d7a007a155d338
Zeynepozden34 <114918310+Zeynepozden34@users.noreply.github.com>
2022-12-09 18:12:28 +0300

    9.12.2022

---
+++ Hafta10/09-12-2022/ShoppingApp/ShoppingApp.Core/Jobs.cs
@@ -0,0 +1,52 @@
+﻿namespace ShoppingApp.Core
+{
+    public static class Jobs //URL OLUŞTURULACAK.
+    {
+
+        public static string InitUrL(string url)
+        {
+            #region Açıklamalar
+            /* BU metot kendisine gelen url değişkeninin
+            1 - Latin alfabesine çevrilen problem çıkaran İ-i ı-i gibi dönüşünleri yapacak
+            2-türkçe karakter yerine latin albesindeki karşılıklarını koyacak
+            3-Boşluklar yerine - koyacak
+            4-(.),(/) gibi karakterleri kaldıracak
+            */
+            #endregion
+            #region Sorunlu karakterler düzeltiliyor
+            url = url.Replace("I", "i");
+            url = url.Replace("İ", "i");
+            url = url.Replace("ı", "i");
+            #endregion
+
+            #region küçük harfe dönüştürüldü
+            url = url.ToLower();
+            #endregion
+
+            #region Türkçe karakterleri dönüştürdük
+            url = url.Replace("ö", "o");
+            url = url.Replace("ü", "u");
+            url = url.Replace("ş", "s");
+            url = url.Replace("ç", "c");
+            url = url.Replace("ğ", "g");
+            #endregion
+            #region Boşuklar tire ile değiştirildi
+            url = url.Replace(" ", "-");
+            #endregion
+            #region Sorunlu karakterler kaldırılıyor           
+            url = url.Replace(".", "");
+            url = url.Replace("/", "");
+            url = url.Replace("\"", "");
+            url = url.Replace("'", "");
+            url = url.Replace("(", "");
+            url = url.Replace(")", "");
+            url = url.Replace("[", "");
+            url = url.Replace("]", "");
+            url = url.Replace("{", "");
+            url = url.Replace("}", "");
+            #endregion
+            return url;
+
+        }
+    }
+}
---
+++ Hafta10/09-12-2022/ShoppingApp/ShoppingApp.Web/Areas/Admin/Models/Dtos/ProductListDto.cs
@@ -0,0 +1,10 @@
+﻿using ShoppingApp.Entity.Concrete;
+
+namespace ShoppingApp.Web.Areas.Admin.Models.Dtos
+{
+    public class ProductListDto
+    {
+        // Liste halinde getirmeme nedeni kolaylık olsun diye product'ın hepsini getirdik.
+        public Product Product { get; set; }
+    }
+}
