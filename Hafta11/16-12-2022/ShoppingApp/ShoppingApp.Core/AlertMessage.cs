14876eef0f354d24c5817c46e99208221dc12aeb
Zeynepozden34 <114918310+Zeynepozden34@users.noreply.github.com>
2022-12-16 18:07:38 +0300

    16-12-2022

---
+++ Hafta11/16-12-2022/ShoppingApp/ShoppingApp.Core/AlertMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.Core
+{// Uygulama içinde çeşitli durumlarda ihityaç duyduğumuz uyarı mesajları kullanılacak bir tip.
+    public class AlertMessage
+    {
+        public string Title { get; set; } // uyarı başlıpı
+        public string Message { get; set; } // uyarı mesajı
+        public string AlertType { get; set; } // uyarı mesaj tipi, görünümü
+    }
+}
