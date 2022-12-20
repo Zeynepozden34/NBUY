f8249b1cdc0c8c299c78501a3cf7f1a0d433b1dd
Zeynepozden34 <114918310+Zeynepozden34@users.noreply.github.com>
2022-12-20 18:10:58 +0300

    20-12-2022

---
+++ Hafta12/20-12-2022/ShoppingApp/ShoppingApp.Web/Areas/Admin/Models/Dtos/UserAddDto.cs
@@ -0,0 +1,18 @@
+﻿using ShoppingApp.Entity.Concrete.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace ShoppingApp.Web.Areas.Admin.Models.Dtos
+{
+    public class UserAddDto
+    {
+        public UserDto UserDto { get; set; }
+        public List<RoleDto> Roles { get; set; }
+
+        [DisplayName("Rol")]
+        [Required(ErrorMessage = "{0} alanı zorunludur.")]
+        public IList<string> SelectedRoles { get; set; }
+
+    }
+}
