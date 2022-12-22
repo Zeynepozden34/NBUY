93a0797239f276270e24a19fe4de791089c8dcd1
Zeynepozden34 <114918310+Zeynepozden34@users.noreply.github.com>
2022-12-22 18:15:04 +0300

    22.12.2022

---
+++ Hafta12/22-12-2022 - Card/ShoppingApp/ShoppingApp.Web/Models/Dtos/OrderDto.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace ShoppingApp.Web.Models.Dtos
+{
+    public class OrderDto  // Kresi kartın üstündeki bilgiler.
+    {
+        [DisplayName("Ad")]
+        [Required(ErrorMessage = "{0} alanı zorunludur.")]
+        public string FirstName { get; set; }
+
+
+        [DisplayName("Soyad")]
+        [Required(ErrorMessage = "{0} alanı zorunludur.")]
+        public string LastName { get; set; }
+
+        [DisplayName("Adres")]
+        [Required(ErrorMessage = "{0} alanı zorunludur.")]
+        public string Address { get; set; }
+
+        [DisplayName("Şehir")]
+        [Required(ErrorMessage = "{0} alanı zorunludur.")]
+        public string City { get; set; }
+
+        [DisplayName("Telefon Numarası")]
+        [Required(ErrorMessage = "{0} alanı zorunludur.")]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [DisplayName("Email")]
+        [Required(ErrorMessage = "{0} alanı zorunludur.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [DisplayName("Kart Sahibi")]
+        [Required(ErrorMessage = "{0} alanı zorunludur.")]
+        public string CardName { get; set; }
+
+        [DisplayName("Kart Numarası")]
+        [Required(ErrorMessage = "{0} alanı zorunludur.")]
+        public string CardNumber { get; set; }
+
+        [DisplayName("Geçerlilik Tarihi Ay")]
+        [Required(ErrorMessage = "{0} alanı zorunludur.")]
+        public string ExpirationMonth { get; set; }
+
+        [DisplayName("Geçerlilik Tarihi Yıl")]
+        [Required(ErrorMessage = "{0} alanı zorunludur.")]
+        public string ExpirationYear { get; set;}
+
+        [DisplayName("Üç haneli CVC")]
+        [Required(ErrorMessage = "{0} alanı zorunludur.")]
+        public string Cvc { get; set; }
+
+        public CardDto CardDto { get; set; }
+            
+    }
+}
