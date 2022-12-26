fc88cca7870368fe148e5f5a8f02970ad7b6ed37
Zeynepozden34 <114918310+Zeynepozden34@users.noreply.github.com>
2022-12-26 18:06:15 +0300

    26-12-2022

---
+++ Hafta13/26-12-2022-SqlServereGecis/ShoppingApp/ShoppingApp.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingApp.Business.Abstract;
+using ShoppingApp.Business.Concrete;
+using ShoppingApp.Entity.Concrete;
+using ShoppingApp.Web.Models.Dtos;
+
+namespace ShoppingApp.Web.Areas.Admin.Controllers
+{
+    [Authorize]
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly IProductService _productManager;
+
+        public HomeController(IProductService productManager)
+        {
+            _productManager = productManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+   
+    }
+}
---
+++ Hafta13/26-12-2022-SqlServereGecis/ShoppingApp/ShoppingApp.Web/Models/Extension/MigrationManager.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingApp.Data.Concrete.EfCore.Contexts;
+
+namespace ShoppingApp.Web.Models.Extension
+{
+    public static  class MigrationManager 
+    {
+        public static IHost UpdateDatabase(this IHost host) //sHOPAPP CONTEXT TİPİNDE YENİ BİR SCOP YARATTUIK.
+        {
+            using (var scope=host.Services.CreateScope()) //builder.services gibi yeni bir scop yarattık.
+            {
+                using (var shopAppContext =scope.ServiceProvider.GetRequiredService<ShopAppContext>())
+                {
+                    try
+                    {
+                        shopAppContext.Database.Migrate();//bu komut henüz işlenmemiş migrationları alır ve database update işlemine tabi tutar
+                    }
+                    catch (Exception)
+                    {
+                        //hataların bulunduğu loglar burada hazırlanır.
+                        throw;
+                    }
+                }
+            }
+            return host;
+        }
+    }
+    
+}
