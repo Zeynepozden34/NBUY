b82b1062ae9cd2d57b27a2485d59592625c146cd
Zeynepozden34 <zeynepozden01@gmail.com>
2022-12-27 10:12:20 +0300

    hazırlık

---
+++ Hafta13/27-12-2022/ShoppingApp/ShoppingApp.Web/Areas/Admin/Models/Dtos/CategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingApp.Web.Areas.Admin.Models.Dtos
+{
+    public class CategoryDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
